d63be9fb935428189901af5d50ac09eeeb3f326a
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-12-27 12:01:17 -0800

    add the client views

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,9 +2,9 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    public int clientId { get; set; }
-    public string clientname { get; set; }
-    public int stylistId { get; set; }
+    public int ClientId { get; set; }
+    public string ClientName { get; set; }
+    public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
 }
