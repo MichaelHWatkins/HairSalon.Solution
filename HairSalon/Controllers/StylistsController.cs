0e1c42b47298339202ecb8e5c6fcc91d7134d93b
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-12-27 10:31:18 -0800

    add the controllers

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    private readonly ToDoListContext _db;
+
+    public CategoriesController(ToDoListContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Category> model = _db.Categories.ToList();
+      return View(model);
+    }
+    
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Category category)
+    {
+        _db.Categories.Add(category);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+        Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+        return View(thisCategory);
+    }
+  }
+}
